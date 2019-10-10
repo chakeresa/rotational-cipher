2ab49b72736495fc544c96a85bf8cf723f07ec05
Alexandra Chakeres <alexchakeres@gmail.com>
2019-10-09 22:46:16 -0600

    Add shift_letter helper method

--- lib/rotational_cipher.ex
+++ lib/rotational_cipher.ex
@@ -23,20 +23,24 @@ defmodule RotationalCipher do
   @spec shift_char(char :: char, shift :: integer) :: char
   defp shift_char(char, shift) do
     cond do
-      within_bounds(char, @lowercase_bounds) ->
-        char + shift - 97
-        |> rem(26)
-        |> Kernel.+(97)
-      within_bounds(char, @uppercase_bounds) ->
-        char + shift - 65
-        |> rem(26)
-        |> Kernel.+(65)
+      within_bounds?(char, @lowercase_bounds) ->
+        shift_letter(char, shift, @lowercase_bounds)
+      within_bounds?(char, @uppercase_bounds) ->
+        shift_letter(char, shift, @uppercase_bounds)
       true -> char
     end
   end
 
-  @spec within_bounds(number :: integer, list :: [integer]) :: boolean
-  defp within_bounds(number, list) do
+  @spec within_bounds?(number :: integer, list :: [integer]) :: boolean
+  defp within_bounds?(number, list) do
     number <= Enum.at(list, 1) && number >= Enum.at(list, 0)
+  end
+
+  @spec shift_letter(char :: char, shift :: integer, case_bounds :: [integer]) :: char
+  defp shift_letter(char, shift, case_bounds) do
+    start_of_range = Enum.at(case_bounds, 0)
+    char + shift - start_of_range
+    |> rem(26)
+    |> Kernel.+(start_of_range)
   end
 end
